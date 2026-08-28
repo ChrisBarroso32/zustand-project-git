cd2d623adc2ec3ab462438838012020212f8160b
Christian <chrisbarroso32@gmail.com>
2026-08-27 20:35:01 -0600

    Se instaló y configuró TailwindCSS en el proyecto.

--- zustand-project/src/App.jsx
+++ zustand-project/src/App.jsx
@@ -7,10 +7,22 @@ const App = () => {
     const [cart, setCart] = useState([])
 
     return (
-        <div className="App">
-            <h3>¡Bienvenido a la tienda virtual de carros!</h3>
-            <ProductList products={PRODUCTS} setCart={setCart} />
-            <Cart setCart={setCart} cart={cart} />
+        <div className="min-h-screen bg-gray-100 p-8 font-sans">
+            <div className="max-w-7xl mx-auto">
+                <h1 className="text-4xl font-extrabold text-center text-gray-800 mb-10 tracking-tight">
+                    Catálogo de Carros
+                </h1>
+                <div className="flex flex-col lg:flex-row gap-8">
+
+                    <div className="w-full lg:w-3/4">
+                        <ProductList products={PRODUCTS} setCart={setCart} />
+                    </div>
+
+                    <div className="w-full lg:w-1/4">
+                        <Cart setCart={setCart} cart={cart} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
