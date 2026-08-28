99b203f67ff31c67d7ca60a1d21a7d146fd8b4e8
Christian <chrisbarroso32@gmail.com>
2026-08-28 14:36:14 -0600

    Se generó el script createSelectors.js y se modificaron App.jsx y store.js

--- zustand-project/src/App.jsx
+++ zustand-project/src/App.jsx
@@ -1,27 +1,17 @@
-import Cart from "./components/Cart.jsx";
-import ProductList from "./components/ProductList.jsx";
-import { PRODUCTS } from './products.js'
+import {useStoreSelectors} from "./store/store.js";
 
-const App = () => {
+const Counter = () => {
+    const count = useStoreSelectors.use.count();
+    const increment = useStoreSelectors.use.increment();
+    const decrement = useStoreSelectors.use.decrement();
+
     return (
-        <div className="min-h-screen bg-gray-800 p-8 font-sans">
-            <div className="max-w-7xl mx-auto">
-                <h1 className="text-4xl font-extrabold text-center text-gray-300 mb-10 tracking-tight">
-                    Catálogo de Carros
-                </h1>
-
-                <div className="flex flex-col lg:flex-row gap-8">
-
-                    <div className="w-full lg:w-3/4">
-                        <ProductList products={PRODUCTS}/>
-                    </div>
-
-                    <div className="w-full lg:w-1/4">
-                        <Cart/>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <h2>Count: {count}</h2>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
         </div>
-    )
-}
-export default App;
+    );
+};
+
+export default Counter;
--- zustand-project/src/store/store.js
+++ zustand-project/src/store/store.js
@@ -1,26 +1,12 @@
 import {create} from "zustand";
-import {produce} from "immer";
+import {createSelectors} from "./createSelectors.js"
+// import {produce} from "immer";
 
-const initialState = {
-    user: {
-        id: "user123",
-        friends: ["Latife", "Javier", "Darío", "Bryan"],
-        profile: {
-            name: " Christian Barroso",
-            email: "chrisBaRa@example.com",
-            address: {
-                street: "Calle Totonilco 152",
-                city: "CDMX",
-                zipCode: "12345",
-            },
-        },
-    },
-};
-
-export const useStore = create((set) => ({
-    ...initialState,
-    updateAdressStreet: (street) =>
-        set(produce((state) => {
-        state.user.profile.address.street = street;
-    })),
+const useStore = create((set) => ({
+    count: 0,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+    decrement: () => set((state) => ({ count: state.count - 1 })),
 }));
+
+export const useStoreSelectors = createSelectors(useStore);
+export default useStore;
